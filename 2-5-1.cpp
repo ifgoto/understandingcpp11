7fcf3276e929d48f1b67d8a2a889f0d977a9fc61
hammer <twitter9mail@gmail.com>
2017-03-20 22:15:41 +0800

    add the file

---
+++ 2-5-1.cpp
@@ -0,0 +1,17 @@
+#include <cassert>
+using namespace std;
+
+
+//if deifne NDEBUG here would not nullable to assert
+//define NDEBUG 1
+
+//
+//we should g++ -DNDEBUG ..........
+char* ArrayAlloc(int n) {
+    assert(n > 0);
+    return new char[n];
+
+}
+int main(){
+    char* a = ArrayAlloc(0);
+};
